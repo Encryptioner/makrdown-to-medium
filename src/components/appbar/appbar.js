3420b74fc87be131706438193c87746b9e06b642
Ankur Mursalin <mir.ankur.ruet13@gmail.com>
2025-10-22 23:07:49 +0600

    feat: add logo in header

--- src/components/appbar/appbar.js
+++ src/components/appbar/appbar.js
@@ -2,11 +2,22 @@ import React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
 
 const Bar = () => {
   return (
     <AppBar position="static">
       <Toolbar>
+        <Box
+          component="img"
+          src={`${process.env.PUBLIC_URL}/logo-white.svg`}
+          alt="Markdown to Medium Logo"
+          sx={{
+            height: 40,
+            width: 40,
+            mr: 2,
+          }}
+        />
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
           Markdown to Medium
         </Typography>
